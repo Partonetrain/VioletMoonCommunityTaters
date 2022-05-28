1130f4611ca1623ca2f6051ce16e8a34ba85e4de
Partonetrain <Partonetrain@gmail.com>
2022-05-28 14:57:13 -0500

    Add pack.png to build.sh

--- build.sh
+++ build.sh
@@ -4,13 +4,13 @@ set -x
 rm -f vm_community_taters_quark.zip
 rm -f vm_community_taters_botania.zip
 
-zip -r vm_community_taters_quark.zip assets pack.mcmeta
+zip -r vm_community_taters_quark.zip assets pack.mcmeta pack.png
 
 TMP=$(mktemp -d)
 cp -r assets pack.mcmeta "$TMP"
 mv "$TMP/assets/quark" "$TMP/assets/botania"
 find "$TMP" -name "*.json" -exec sed -i 's/quark:/botania:/' {} +
-(cd "$TMP" && zip -r vm_community_taters_botania.zip assets pack.mcmeta)
+(cd "$TMP" && zip -r vm_community_taters_botania.zip assets pack.mcmeta pack.png)
 
 cp -f "$TMP"/vm_community_taters_botania.zip .
 rm -rf "$TMP"
