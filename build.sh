01c0c723467c903ba23708a4ef774523de3156cc
Partonetrain <Partonetrain@gmail.com>
2022-05-28 15:01:29 -0500

    <--- forgot a line in the build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ rm -f vm_community_taters_botania.zip
 zip -r vm_community_taters_quark.zip assets pack.mcmeta pack.png
 
 TMP=$(mktemp -d)
-cp -r assets pack.mcmeta "$TMP"
+cp -r assets pack.mcmeta pack.png "$TMP"
 mv "$TMP/assets/quark" "$TMP/assets/botania"
 find "$TMP" -name "*.json" -exec sed -i 's/quark:/botania:/' {} +
 (cd "$TMP" && zip -r vm_community_taters_botania.zip assets pack.mcmeta pack.png)
